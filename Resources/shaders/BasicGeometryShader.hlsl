809059d75279eeb22ac3927ce7c0a06e7a1bdd11
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-10-31 17:09:56 +0900

    02_03ヨシッ

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,17 +1,22 @@
 #include "BasicShaderHeader.hlsli"
-
+// 三角形の入力から点一つ出力するサンプル
 [maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
+	point VSOutput input[1] : SV_POSITION, 
+	// 点ストリーム
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
+	GSOutput element;
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+
+	element.svpos = input[0].svpos;
+	output.Append(element);
+
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
+	output.Append(element);
+
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 }
