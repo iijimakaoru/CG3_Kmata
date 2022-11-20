d3632f6568707929aff57571a790e47263920ccd
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-11-20 17:36:27 +0900

    GSshaderコピペ

--- BasicGS.hlsl
+++ BasicGS.hlsl
@@ -1,18 +1,38 @@
-struct GSOutput
+#include "Basic.hlsli"
+
+// 三角形の入力から点一つ出力するサンプル
+static const uint vnum = 4;
+
+static const float4 offset_array[vnum] =
 {
-	float4 pos : SV_POSITION;
+	float4(-0.5f,-0.5f,0,0),
+	float4(-0.5f,+0.5f,0,0),
+	float4(+0.5f,-0.5f,0,0),
+	float4(+0.5f,+0.5f,0,0),
 };
 
-[maxvertexcount(3)]
+static const float2 uv_array[vnum] =
+{
+	float2(0,1),
+	float2(0,0),
+	float2(1,1),
+	float2(1,0),
+};
+
+[maxvertexcount(vnum)]
 void main(
-	triangle float4 input[3] : SV_POSITION, 
+	point VSOutput input[1] : SV_POSITION,
+	// 点ストリーム
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
+	GSOutput element;
+	for (uint i = 0; i < vnum; i++)
 	{
-		GSOutput element;
-		element.pos = input[i];
+		element.svpos = input[0].pos + offset_array[i];
+
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
 		output.Append(element);
 	}
 }
